da1421345be6e519e6b25996335dccbffc3e1a80
Giorgi Kveniashvili <giorgikveniashvili@gmail.com>
2023-05-16 12:50:00 +0400

    rightmove pagination

--- property_scraper.py
+++ property_scraper.py
@@ -1,4 +1,3 @@
-import requests
 import openpyxl
 import time
 from bs4 import BeautifulSoup
@@ -8,27 +7,38 @@ from selenium.webdriver.common.by import By
 excel_file_location = 'Properties.xlsx'
 
 class RightMoveScraper:
+    location = 'REGION%5E87490'
+    min_price = '80000'
+    max_price = '80000'
     excel_sheet = 'rightmove'
     row_index = 2
-    website_page_index = 0
-    max_page_index = 0
-    website_url = 'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E87490&maxPrice=450000&minPrice=350000&index={page_index}&includeSSTC=false'
-    headers = {'Sec-Ch-Ua':'Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+    website_url = 'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={location}&maxPrice={max_price}&minPrice={min_price}&includeSSTC=false'
     
     def do_scrape(self):
         print('Scraping rightmove.co.uk')
 
-        page_content = requests.get(self.website_url.format(page_index=self.website_page_index), headers=self.headers)
-        page_soup = BeautifulSoup(page_content.content, 'html.parser')
-        page_listings = page_soup.find_all('a', class_='propertyCard-anchor')
-        max_page_index = page_soup.find_all('span', class_='pagination-pageInfo')
+        dr = webdriver.Chrome()
+        dr.get(self.website_url.format(location=self.location, max_price=self.max_price, min_price=self.min_price))
+        time.sleep(1)
 
-        for listing in page_listings:
-            listing_url = self.convert_url(listing['id'])
-            write_excel(self.excel_sheet, self.row_index, listing_url)
-            self.row_index += 1
-        
+        while True:
+            page_soup = BeautifulSoup(dr.page_source, 'html.parser')
+            page_listings = page_soup.find_all('a', class_='propertyCard-anchor')
+
+            for listing in page_listings:
+                listing_url = self.convert_url(listing['id'])
+                write_excel(self.excel_sheet, self.row_index, listing_url)
+                self.row_index += 1
+            
+            try:
+                next_button = dr.find_element(By.CLASS_NAME, 'pagination-direction--next')
+            except:
+                break
+            if not next_button.is_enabled():
+                break
+
+            dr.execute_script("arguments[0].click();", next_button)
+            time.sleep(2)
 
     def convert_url(self, href):
         base_url = 'https://www.rightmove.co.uk/properties/'
@@ -54,6 +64,8 @@ class ZooplaScraper:
             listing_url = self.convert_url(listing['href'])
             write_excel(self.excel_sheet, self.row_index, listing_url)
             self.row_index += 1
+        
+        dr.close()
     
     def convert_url(self, href):
         base_url = 'https://www.zoopla.co.uk'
@@ -98,7 +110,8 @@ class HalmanScraper:
                 time.sleep(4)
             except:
                 break
-
+        
+        dr.close()
         
         page_soup = BeautifulSoup(dr.page_source, 'html.parser')
         page_listings = page_soup.find_all('a', class_='btn btn-red')
@@ -122,14 +135,14 @@ def write_excel(sheet_name, sheet_index, url):
     wb.close()
 
 def main():
-    """rightmove_scraper = RightMoveScraper()
+    rightmove_scraper = RightMoveScraper()
     rightmove_scraper.do_scrape()
 
-    zoopla_scraper = ZooplaScraper()
-    zoopla_scraper.do_scrape()"""
+    """zoopla_scraper = ZooplaScraper()
+    zoopla_scraper.do_scrape()
 
     halman_scraper = HalmanScraper()
-    halman_scraper.do_scrape()
+    halman_scraper.do_scrape()"""
 
 if __name__ == "__main__":
     main()
