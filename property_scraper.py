7d314d7612a72ba39dabcdf0771501503f44c1b6
Giorgi Kveniashvili <giorgikveniashvili@gmail.com>
2023-05-18 12:54:05 +0400

    zoopla paging

--- property_scraper.py
+++ property_scraper.py
@@ -12,7 +12,7 @@ class RightMoveScraper:
     # Editable parameters
     location = 'REGION%5E87490'
     min_price = '350000'
-    max_price = '350000'
+    max_price = '450000'
 
     website_url = 'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier={prop_location}&maxPrice={prop_max_price}&minPrice={prop_min_price}&includeSSTC=false'.format(prop_location=location, prop_max_price=max_price, prop_min_price=min_price)
     excel_sheet = 'rightmove'
@@ -22,7 +22,7 @@ class RightMoveScraper:
         print('Scraping rightmove.co.uk')
 
         dr = webdriver.Chrome()
-        dr.get(self.website_url.format(location=self.location, max_price=self.max_price, min_price=self.min_price))
+        dr.get(self.website_url)
         time.sleep(1)
 
         while True:
@@ -51,31 +51,57 @@ class RightMoveScraper:
         return base_url + href[4:]
 
 class ZooplaScraper:
+    location = 'london'
+    min_price = '350000'
+    max_price = '450000'
+    
+    website_url_getter = 'https://www.zoopla.co.uk/api/search/resolver/?price_frequency=per_month&results_sort=newest_listings&new_homes=include&retirement_homes=true&shared_ownership=true&include_shared_accommodation=true&search_source=for-sale&section=for-sale&view_type=list&price_max={prop_max_price}&price_min={prop_min_price}&q={prop_query1}&orig_q={prop_query2}'.format(prop_max_price=max_price, prop_min_price=min_price, prop_query1=location, prop_query2=location)
+    page_number = 1
     excel_sheet = 'zoopla'
     row_index = 2
-    website_page_index = 0
-    max_page_index = 24
-    website_url = 'https://www.zoopla.co.uk/for-sale/property/cheshire/?price_max=450000&price_min=350000&q=Cheshire&results_sort=newest_listings&search_source=home'
-    
+
     def do_scrape(self):
         print('Scraping zoopla.co.uk')
 
         dr = webdriver.Chrome()
-        dr.get(self.website_url)
+        dr.get(self.website_url_getter)
+        website_uri = dr.find_element(By.TAG_NAME, 'pre').text
+        dr.close()
         time.sleep(1)
-        page_soup = BeautifulSoup(dr.page_source, 'html.parser')
-        page_listings = page_soup.find_all('a', class_='_1maljyt1')
 
-        for listing in page_listings:
-            listing_url = self.convert_url(listing['href'])
-            write_excel(self.excel_sheet, self.row_index, listing_url)
-            self.row_index += 1
+        while True:
+            dr = webdriver.Chrome()
+            website_url = 'https://www.zoopla.co.uk{uri}&pn={page_number}'.format(uri=website_uri, page_number=self.page_number)
+            print(website_url)
+            print(self.page_number)
+
+            dr.get(website_url)
+            time.sleep(3)
+
+            page_soup = BeautifulSoup(dr.page_source, 'html.parser')
+            page_listings = page_soup.find_all('a', class_='_1maljyt1')
+
+            for listing in page_listings:
+                listing_url = self.convert_url(listing['href'])
+                write_excel(self.excel_sheet, self.row_index, listing_url)
+                self.row_index += 1
+            
+            try:
+                nav_menu = dr.find_element(By.CLASS_NAME, '_13wnc6k0')
+                next_button = nav_menu.find_element(By.CLASS_NAME, '_1ljm00us').find_element(By.TAG_NAME, 'a')
+            except:
+                break
+            if next_button.get_attribute('aria-disabled') == 'true':
+                break
+
+            self.page_number += 1
+            dr.close()
         
         dr.close()
     
     def convert_url(self, href):
         base_url = 'https://www.zoopla.co.uk'
-        return base_url + href
+        return base_url + href.split('/?')[0]
     
 class HalmanScraper:
     website_url = 'https://www.gascoignehalman.co.uk/search/?showstc=on&showsold=on&instruction_type=Sale&place=cheadle&ajax_border_miles=1&minprice=350000&maxprice=450000'
@@ -155,16 +181,16 @@ def create_excel():
     wb.close()
 
 def main():
-    create_excel()
+    """create_excel()
 
     rightmove_scraper = RightMoveScraper()
-    rightmove_scraper.do_scrape()
+    rightmove_scraper.do_scrape()"""
 
     zoopla_scraper = ZooplaScraper()
     zoopla_scraper.do_scrape()
 
-    halman_scraper = HalmanScraper()
-    halman_scraper.do_scrape()
+    #halman_scraper = HalmanScraper()
+    #halman_scraper.do_scrape()
 
 if __name__ == "__main__":
     main()
